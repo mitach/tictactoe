2d28ff8928a7962c936d903676129e294c88e066
Dimitar Malinov <dimitar.malinov@amusnet.com>
2022-10-27 16:24:19 +0300

    socket.io: tic-tac-toe

---
+++ server.js
@@ -0,0 +1,63 @@
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+
+const app = express();
+app.use('/', express.static('static'));
+const server = http.createServer(app);
+const io = socketIO(server);
+
+const rooms = {};
+
+io.on('connect', socket => {
+    console.log('Player connected!');
+    
+    socket.on('selectRoom', roomId => {
+        if (rooms[roomId] == undefined) {
+            rooms[roomId] = new Map();
+        }
+
+        const players = rooms[roomId];
+
+        if (players.size >= 2) {
+            socket.emit('error', 'Room is full!');
+            socket.disconnect();
+        } else {
+            socket.join(roomId);
+            initGame(roomId, players, socket);
+        }
+    });
+});
+
+
+function initGame(roomId, players, socket) {
+    
+    socket.on('position', pos => {
+        console.log('Position', pos);
+        io.to(roomId).emit('position', pos)
+    });
+
+    socket.on('newGame', () => {
+        console.log('New game initiated!');
+        io.to(roomId).emit('newGame')
+    });
+
+    socket.on('disconnect', () => {
+        console.log('PLayer left!');
+        players.delete(socket);
+    })
+    
+    let symbol = 'X';
+    if (players.size > 0) {
+        const otherSymbol = [...players.values()][0];
+
+        if (otherSymbol == 'X') {
+            symbol = 'O'
+        }
+    }
+    players.set(socket, symbol);
+    console.log('Assigning symbol', symbol);
+    socket.emit('symbol', symbol);
+}
+
+server.listen(5000, () => console.log('Server listening on port 5000'));
